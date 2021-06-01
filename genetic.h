35e952009e54ac90ac3f7c237bb1ebdc937baaee
Hawkaoaoa <78674440+Hawkaoaoa@users.noreply.github.com>
2021-06-01 22:09:08 +0800

    Renew

--- genetic.h
+++ genetic.h
@@ -2,11 +2,13 @@
 # include <stdlib.h>
 # include <time.h>
 # include <string.h>
+# include "mpi.h"
+# include <unistd.h>
 
-#define CROSS 0.6   /* 交叉 */
-#define MUTATE 0.05    /* 变异 */
-#define NUM 30   /* 每一代的个体数 */
-#define MAXITER 20  /* 计数器， 最大适应度不变的次数 （停止准则） */
+#define CROSS 0.4   /* 交叉 */
+#define MUTATE 0.2  /* 变异 */
+#define MAXITER 100 /* 计数器， 最大适应度不变的次数 （停止准则） */
+int NUM = 2000; /* 每一代的个体数, 在主程序中会被划分，交给每个子进程处理！ */
 
 typedef enum {false,true} bool;
 
