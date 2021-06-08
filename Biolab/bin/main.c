af50123cad85bfd823c44ffd6d69a5a7c237cf47
lirunhan <lirunhan@sjtu.edu.cn>
2021-06-09 00:23:29 +0800

    user friendly

--- Biolab/bin/main.c
+++ Biolab/bin/main.c
@@ -11,6 +11,24 @@ int main(int argc, char *argv[])
   FILE *fp1 = fopen((char *)argv[2], "rt");
   FILE *fp2 = fopen((char *)argv[3],"wt");
  
+  if(argc!=4){
+	printf("Wrong command line parameter!\n");
+	printf("$0<INPUT><OUTPUT>\n");
+	return -1;
+}
+
+ if (fp1 == NULL || fp2 == NULL){
+    perror("Error while opening the file.\n");
+    exit(-1);
+  }
+
+ if(strcmp((char *)argv[1],"-d1") != 0 && strcmp((char *)argv[1],"-d2") != 0\
+&& strcmp((char *)argv[1],"-w") !=0){
+	printf("Wrong options!\n");
+	printf("The options are -d1 -d2 or -w.\n");
+	return -1;
+}
+
   if(strcmp((char *)argv[1],"-d1") == 0)
     {
       Dynamic1(fp1, fp2, argv[2], argv[3]);
